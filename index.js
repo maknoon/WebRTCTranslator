3bda7c68ad993140a4bbe17ecf4187baa55a40a8
maknoon <conniemob@gmail.com>
2018-01-14 07:55:41 -0800

    Live testing

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const Translate = require('@google-cloud/translate');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/app/views/index.html');
 });
 
-io.on('connection', function(socket) {
+io.on('connection', async socket => {
     console.log('a user connected');
 
-    socket.on('chat message', function(msg) {
-        const Translate = require('@google-cloud/translate');
+    socket.on('chat message', async(msg) => {
         const translate = new Translate();
-        // var text = 'Hello World';
-        var text = msg;
         var target = 'zh-TW';
+
         translate
-            .translate(text, target)
+            .translate(msg, target)
             .then(results => {
                 let translations = results[0];
                 translations = Array.isArray(translations)
                   ? translations
                   : [translations];
                 io.emit('chat message', 'Translations: ');
-                // console.log('Translations:');
+                console.log('Translations:');
                 translations.forEach((translation, i) => {
                     io.emit('chat message', `${text[i]} => (${target}) ${translation}`);
-                    // console.log(`${text[i]} => (${target}) ${translation}`);
+                    console.log(`${text[i]} => (${target}) ${translation}`);
                 });
             })
             .catch(err => {
@@ -38,11 +37,11 @@ io.on('connection', function(socket) {
         // console.log('message: ' + msg);
     });
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
 
-http.listen(3000, function() {
+http.listen(3000, () => {
     console.log('listening on *:3000');
 });
